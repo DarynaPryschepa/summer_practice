8108e1a0c83ea173a409ea07799b9a92d33da6c1
DarynaPryschepa <prischepadarina@gmail.com>
2021-06-16 14:10:46 +0300

    SP-29:Addition of dabase initialization

---
+++ src/main/resources/db/migration/V2__Add_Data.sql
@@ -0,0 +1,7 @@
+
+
+insert into working_place (id, created_at, created_by,updated_at, updated_by,  name, city)
+values (1,now(),'Daryna',now(),'Daryna','Working_place_211','Chernihiv');
+insert into monitor (id, created_at, created_by, updated_at, updated_by, length, height,
+                     width, vesa, display_size,  w_place_id) values (1, now(), 'Samsung',now(),'Jack',
+                                                                     600,800,100,'600 x 800',456,1);
